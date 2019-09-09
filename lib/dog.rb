a67f78397813dfd71fdd2dc2f9fbb16c97959090
Anthony Oglesby <ogle1ant@gmail.com>
2019-09-09 14:10:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,22 +1,30 @@
 class Dog
-    def initialize(name)
+  def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
-  end
-  
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
   end
 end
+
+
+# class Dog
+#     def initialize(name)
+#     @name = name
+#     @breed = breed
+#   end
+  
+#   def breed=(breed)
+#     @breed = breed
+#   end
+  
+#   def breed
+#     @breed
+#   end
+  
+#   def name=(name)
+#     @name = name
+#   end
+  
+#   def name
+#     @name
+#   end
+# end
